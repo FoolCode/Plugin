be985a9d788a66a3a0ffa187f77200b42e07119b
woxxy <woxxy@foolrulez.org>
2012-11-30 17:03:20 +0100

    Adapting to new loader package

--- classes/Foolz/Plugin/Loader.php
+++ classes/Foolz/Plugin/Loader.php
@@ -9,272 +9,23 @@ namespace Foolz\Plugin;
  * @package  Foolz\Plugin
  * @license  http://www.apache.org/licenses/LICENSE-2.0.html Apache License 2.0
  */
-class Loader
+class Loader extends \Foolz\Package\Loader
 {
 	/**
-	 * The instances of the Loader class
+	 * The type of package in use. Can be in example 'theme' or 'plugin'
+	 * Override this to change type of package
 	 *
-	 * @var  \Foolz\Plugin\Loader[]
+	 * @var  string
 	 */
-	protected static $instances = [];
+	protected $type_name = 'plugin';
 
 	/**
-	 * The dirs in which to look for plugins
+	 * The class into which the resulting objects are created.
+	 * Override this, in example Foolz\Plugin\Plugin or Foolz\Theme\Theme
 	 *
-	 * @var  null|array
+	 * @var  string
 	 */
-	protected $dirs = null;
-
-	/**
-	 * The classes for the autoloader
-	 *
-	 * @var  null|array
-	 */
-	protected $classes = null;
-
-	/**
-	 * The plugins found
-	 *
-	 * @var  null|array  as first key the dir name, as second key the slug
-	 */
-	protected $plugins = null;
-
-	/**
-	 * Tells if plugins should be reloaded
-	 *
-	 * @var  boolean
-	 */
-	protected $reload = true;
-
-	/**
-	 * Construct registers the class to the autoloader
-	 */
-	public function __construct()
-	{
-		$this->register();
-	}
-
-	/**
-	 * Creates or returns a named instance of Loader
-	 *
-	 * @param   string  $instance  The name of the instance to use or create
-	 * @param   bool    $prepend   If the autoloader should be prepended
-	 *
-	 * @return  \Foolz\Plugin\Loader
-	 */
-	public static function forge($instance = 'default', $prepend = false)
-	{
-		if ( ! isset(static::$instances[$instance]))
-		{
-			return static::$instances[$instance] = new static($prepend);
-		}
-
-		return static::$instances[$instance];
-	}
-
-	/**
-	 * Destroys a named instance and unregisters its autoloader
-	 *
-	 * @param  string  $instance  The name of the instance to use or create
-	 */
-	public static function destroy($instance = 'default')
-	{
-		$obj = static::$instances[$instance];
-		$obj->unregister();
-		unset(static::$instances[$instance]);
-	}
-
-	/**
-	 * Registers the current object with spl_autoload_register
-	 *
-	 * @param  bool  $prepend  if the class loader should run first, would allow overriding classes
-	 */
-	protected function register($prepend = false)
-	{
-		spl_autoload_register([$this, 'classLoader'], true, $prepend);
-	}
-
-	/**
-	 * Unregisters the current object with spl_autoload_unregister
-	 */
-	protected function unregister()
-	{
-		spl_autoload_unregister([$this, 'classLoader']);
-	}
-
-	/**
-	 * Class Autoloader function
-	 *
-	 * @param   string  $class  The name of the class to load
-	 *
-	 * @return  void|boolean  True if the class has been found, void otherwise
-	 */
-	public function classLoader($class, $psr = false)
-	{
-		if (isset($this->classes[$class]))
-		{
-			include $this->classes[$class];
-			return true;
-		}
-	}
-
-	/**
-	 * Adds a class to the autoloader
-	 *
-	 * @param   string  $class  The name of the class
-	 * @param   string  $path   The path where the class can be found
-	 *
-	 * @return  \Foolz\Plugin\Loader  The current object
-	 */
-	public function addClass($class, $path)
-	{
-		$this->classes[$class] = $path;
-		return $this;
-	}
-
-	/**
-	 * Returns the path of the class
-	 *
-	 * @param   string  $class  The name of the class
-	 *
-	 * @return  string  The path to the class
-	 * @throws  \OverflowException  If the class hasn't been declared
-	 */
-	public function getClassPath($class)
-	{
-		if ( ! isset($this->classes[$class]))
-		{
-			throw new \OverflowException;
-		}
-
-		return $this->classes[$class];
-	}
-
-	/**
-	 * Removes a class from the autoloader
-	 *
-	 * @param   string  $class  The class to remove
-	 *
-	 * @return  \Foolz\Plugin\Loader  The current object
-	 */
-	public function removeClass($class)
-	{
-		unset($this->classes[$class]);
-		return $this;
-	}
-
-	/**
-	 * Adds a directory to the array of directories to search plugins in
-	 *
-	 * @param   string       $dir_name  If $dir is not set this sets both the name and the dir equal
-	 * @param   null|string  $dir       The dir where to look for plugins
-	 *
-	 * @return  \Foolz\Plugin\Loader  The current object
-	 * @throws  \DomainException      If the directory is not found
-	 */
-	public function addDir($dir_name, $dir = null)
-	{
-		if ($dir === null)
-		{
-			// if $dir is not specified, we use $dir_name as both $dir and $dir_name
-			$dir = $dir_name;
-		}
-
-		if ( ! is_dir($dir))
-		{
-			throw new \DomainException('Directory not found.');
-		}
-
-		$this->dirs[$dir_name] = rtrim($dir,'/').'/';
-
-		// set the flag to reload plugins on demand
-		$this->reload = true;
-
-		return $this;
-	}
-
-	/**
-	 * Removes a dir from the array of directories to search plugins in
-	 * Unsets also all the plugins in that directory
-	 *
-	 * @param   string  $dir_name  The named directory
-	 *
-	 * @return  \Foolz\Plugin\Loader
-	 */
-	public function removeDir($dir_name)
-	{
-		unset($this->dirs[$dir_name]);
-		unset($this->plugins[$dir_name]);
-		return $this;
-	}
-
-	/**
-	 * Looks for plugins in the specified directories and creates the objects
-	 */
-	public function find()
-	{
-		if ($this->plugins === null)
-		{
-			$this->plugins = array();
-		}
-
-		foreach ($this->dirs as $dir_name => $dir)
-		{
-			if ( ! isset($this->plugins[$dir_name]))
-			{
-				$this->plugins[$dir_name] = [];
-			}
-
-			$vendor_paths = $this->findDirs($dir);
-
-			foreach ($vendor_paths as $vendor_name => $vendor_path)
-			{
-				$plugin_paths = $this->findDirs($vendor_path);
-
-				foreach ($plugin_paths as $plugin_name => $plugin_path)
-				{
-					if ( ! isset($this->plugins[$dir_name][$vendor_name.'/'.$plugin_name]))
-					{
-						$plugin = new Plugin($plugin_path);
-						$plugin->setLoader($this);
-						$plugin->setDirName($dir_name);
-						$this->plugins[$dir_name][$vendor_name.'/'.$plugin_name] = $plugin;
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * Internal function to find all directories at the path
-	 *
-	 * @param   string  $path  The path to look into
-	 *
-	 * @return  array   The paths with as they the last part of the path
-	 */
-	protected function findDirs($path)
-	{
-		$result = array();
-		$fp = opendir($path);
-
-		while (false !== ($file = readdir($fp)))
-		{
-			// Remove '.', '..'
-			if (in_array($file, array('.', '..')))
-			{
-				continue;
-			}
-
-			if (is_dir($path.'/'.$file))
-			{
-				$result[$file] = $path.'/'.$file;
-			}
-		}
-
-		closedir($fp);
-
-		return $result;
-	}
+	protected $type_class = 'Foolz\Plugin\Plugin';
 
 	/**
 	 * Gets all the plugins or the plugins from the directory
@@ -286,22 +37,7 @@ class Loader
 	 */
 	public function getAll($dir_name = null)
 	{
-		if ($this->reload === true)
-		{
-			$this->find();
-		}
-
-		if ($dir_name === null)
-		{
-			return $this->plugins;
-		}
-
-		if ( ! isset($this->plugins[$dir_name]))
-		{
-			throw new \OutOfBoundsException('There is no such a directory.');
-		}
-
-		return $this->plugins[$dir_name];
+		return parent::getAll($dir_name);
 	}
 
 	/**
@@ -315,14 +51,6 @@ class Loader
 	 */
 	public function get($dir_name, $slug)
 	{
-		$plugins = $this->getAll();
-
-		if ( ! isset($plugins[$dir_name][$slug]))
-		{
-			throw new \OutOfBoundsException('There is no such a plugin.');
-		}
-
-		$plugins[$dir_name][$slug]->setDirName($dir_name);
-		return $plugins[$dir_name][$slug];
+		return parent::get($dir_name, $slug);
 	}
 }
